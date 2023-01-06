f3887490f4a5606ffa8e6f2678ecf0f64388686b
Sandy Maguire <sandy@sandymaguire.me>
2023-01-05 17:53:57 -0800

    feat: generic resource machinery

--- src/Resources.hs
+++ src/Resources.hs
@@ -1,45 +1,36 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Resources where
 
-import qualified Data.Map as M
-import           Data.Maybe (fromMaybe)
-import           Data.Traversable
+import           Resources.Machinery
+import           SDL (Texture)
 import qualified SDL.Image as Image
-import           System.FilePath
-import           Types
+import           SDL.Mixer (Chunk)
 import qualified SDL.Mixer as Mixer
+import           Types
+
+
+instance IsResource GameTexture Texture where
+  load = Image.loadTexture . e_renderer
+  resourceFolder = "textures"
+  resourceExt    = "png"
+  resourceName NintendoLogo = "nintendo"
+
+instance IsResource Sound Chunk where
+  load _ = Mixer.load
+  resourceFolder = "sounds"
+  resourceExt    = "wav"
+  resourceName NintendoSound = "ding"
 
 
 loadResources :: Engine -> IO Resources
 loadResources engine = do
-  let renderer = e_renderer engine
-
-  textures <- fmap M.fromList $
-    for [minBound @GameTexture .. maxBound] $ \tx-> do
-      let fp = texturePath tx
-      fmap (tx, ) $ Image.loadTexture renderer fp
-
-  sounds <- fmap M.fromList $
-    for [minBound @Sound .. maxBound] $ \tx-> do
-      let fp = soundPath tx
-      fmap (tx, ) $ Mixer.load fp
+  textures <- loadResource engine
+  sounds   <- loadResource engine
 
   pure $ Resources
-    { r_engine = engine
-    , r_textures = fromMaybe (error "missing texture") . flip M.lookup textures
-    , r_sounds = fromMaybe (error "missing sound") . flip M.lookup sounds
+    { r_engine   = engine
+    , r_textures = textures
+    , r_sounds   = sounds
     }
-
-
-textureName :: GameTexture -> FilePath
-textureName NintendoLogo = "nintendo"
-
-texturePath :: GameTexture -> FilePath
-texturePath t = "./resources/textures" </> textureName t <.> "png"
-
-
-soundName :: Sound -> String
-soundName NintendoSound = "ding"
-
-soundPath :: Sound -> FilePath
-soundPath t = "./resources/sounds" </> soundName t <.> "wav"
 
---
+++ src/Resources/Machinery.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE AllowAmbiguousTypes    #-}
+{-# LANGUAGE FunctionalDependencies #-}
+
+module Resources.Machinery where
+
+import qualified Data.Map as M
+import           Data.Traversable (for)
+import           System.FilePath
+import           Types (Engine)
+
+
+class (Ord key, Enum key, Bounded key)
+      => IsResource key res
+       | key -> res
+       , res -> key where
+  resourceFolder :: String
+  resourceExt :: String
+  resourceName :: key -> String
+  load :: Engine -> FilePath -> IO res
+
+
+loadResource
+    :: forall key res
+     . IsResource key res
+    => Engine -> IO (key -> res)
+loadResource engine = do
+  m <- fmap M.fromList $
+    for [minBound @key .. maxBound] $ \k ->
+      fmap (k, ) $ load engine $
+        "resources" </> resourceFolder @key @res </>
+          resourceName k <.> resourceExt @key @res
+  pure $ \k -> m M.! k
+
