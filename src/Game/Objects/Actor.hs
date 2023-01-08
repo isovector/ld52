35bfd25fc73bb8e3e57a7105d530b388cab070d2
Andrew McKnight <thedonk@gmail.com>
2023-01-07 18:51:37 -0800

    Factored out vel stuff

--- src/Game/Objects/Actor.hs
+++ src/Game/Objects/Actor.hs
@@ -19,24 +19,8 @@ actor ore input render pos0 = loopPre (pos0, 0) $
 
     vel'0 <- input -< fi
 
-    let grav = V2 0 10
-
-    let dir = signum $ vel
-    let dir' = signum $ vel'0
-    let maxXSpeed = 100
-    let walkVel = maxXSpeed *^ dir'
     let onGround = touchingGround (l_hitmap lev Layer1 . posToTile) ore pos
-    
-    let freeVel = vel + vel'0 + grav
-    let freeXV = freeVel ^. _x :: Double
-    let maxXV = maxXSpeed * (dir ^. _x) :: Double
-    let cappedXV = (case compare (abs freeXV) maxXSpeed of
-           LT -> freeXV
-           _ -> maxXV)  :: Double
-    let cappedFreeVel = freeVel & _x .~ cappedXV
-    let groundVel = V2 1 0 * walkVel + V2 0 1 * freeVel
-    let vel' = if onGround then groundVel else cappedFreeVel
-
+    let vel' = updateVel onGround vel vel'0 
     let dpos = dt *^ vel'
     let desiredPos = pos + coerce dpos
     let pos' = move (l_hitmap lev Layer1 . posToTile) (coerce ore) pos $ dpos
@@ -62,3 +46,18 @@ actor ore input render pos0 = loopPre (pos0, 0) $
 touchingGround :: (V2 WorldPos -> Bool) -> OriginRect WorldPos -> V2 WorldPos -> Bool
 touchingGround toHit ore pos = let touchDist = V2 0 1
   in or $ fmap toHit (cornersX ore Positive (pos + touchDist))
+
+updateVel :: Bool -> V2 Double -> V2 Double -> V2 Double
+updateVel onGround vel dvel = let
+  grav = V2 0 10
+  dir = signum $ vel
+  dir' = signum $ dvel
+  maxXSpeed = 100
+  walkVel = maxXSpeed *^ dir'
+  freeVel = vel + dvel + grav
+  freeXV = freeVel ^. _x :: Double
+  maxXV = maxXSpeed * (dir ^. _x) :: Double
+  cappedXV = case compare (abs freeXV) maxXSpeed of {LT -> freeXV; _ -> maxXV}
+  cappedFreeVel = freeVel & _x .~ cappedXV
+  groundVel = V2 1 0 * walkVel + V2 0 1 * freeVel
+  in if onGround then groundVel else cappedFreeVel
