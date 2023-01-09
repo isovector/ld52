ca7cc2df5aba3c28c5e58879bf8bf0b599470fcd
Andrew McKnight <thedonk@gmail.com>
2023-01-09 03:55:08 -0800

    Death and CP sounds plus better chicken

--- src/Game/Objects/Checkpoint.hs
+++ src/Game/Objects/Checkpoint.hs
@@ -1,14 +1,19 @@
 module Game.Objects.Checkpoint where
 
-import Game.Common (onHitBy, playerHitRectObjCallback)
+import Game.Common (onHitBy, playerHitRectObj)
 import Types
-
 
 checkpoint :: V2 WorldPos -> Object
 checkpoint pos =
-  playerHitRectObjCallback
-    (fmap (, SetCheckpoint pos) . onHitBy IsPlayer)
+  playerHitRectObj
+    (\oi ->
+      let ev = (fmap (, SetCheckpoint pos) . onHitBy IsPlayer) oi
+       in mempty
+            { oe_die = () <$ ev
+            , oe_play_sound = [CheckpointSound] <$ ev
+            , oe_send_message = fmap pure ev
+            }
+    )
     (OriginRect 8 0)
     (V4 0 255 255 64)
     pos
-
--- src/Game/Objects/Death.hs
+++ src/Game/Objects/Death.hs
@@ -1,15 +1,20 @@
 module Game.Objects.Death where
 
-import Game.Common (onHitBy, playerHitRectObjCallback)
+import Game.Common (onHitBy, playerHitRectObj)
 import Types
-
 
 deathZone :: V2 WorldPos -> V2 Double -> Object
 deathZone pos sz =
-  playerHitRectObjCallback
-    (fmap (, Die) . onHitBy IsPlayer)
+  playerHitRectObj
+    (\oi ->
+      let ev = (fmap (, Die) . onHitBy IsPlayer) oi
+       in mempty
+            { oe_die = () <$ ev
+            , oe_play_sound = [DieSound] <$ ev
+            , oe_send_message = fmap pure ev
+            }
+    )
     (OriginRect (coerce sz) 0)
     (V4 64 0 0 64)
     pos
-
 
