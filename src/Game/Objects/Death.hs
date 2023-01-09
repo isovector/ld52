3d3892cb70ae9a5844ff67b5b00bbb48e8f4d6c9
Andrew McKnight <thedonk@gmail.com>
2023-01-09 05:18:31 -0800

    warm duck shuffle

--- src/Game/Objects/Death.hs
+++ src/Game/Objects/Death.hs
@@ -3,18 +3,29 @@ module Game.Objects.Death where
 import Game.Common (onHitBy, playerHitRectObj)
 import Types
 
+
+-- deathZone :: V2 WorldPos -> V2 Double -> Object
+-- deathZone pos sz =
+--   playerHitRectObjCallback
+--     (fmap (, Die) . onHitBy IsPlayer)
+--     (OriginRect (coerce sz) 0)
+--     (V4 64 0 0 64)
+--     pos
+
+
+
 deathZone :: V2 WorldPos -> V2 Double -> Object
 deathZone pos sz =
   playerHitRectObj
     (\oi ->
       let ev = (fmap (, Die) . onHitBy IsPlayer) oi
        in mempty
-            { oe_die = () <$ ev
-            , oe_play_sound = [DieSound] <$ ev
+            { oe_play_sound = [DieSound] <$ ev
             , oe_send_message = fmap pure ev
             }
     )
     (OriginRect (coerce sz) 0)
     (V4 64 0 0 64)
     pos
+
 
