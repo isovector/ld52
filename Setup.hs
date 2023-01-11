306fdbb27f8a3854b7d3645823ba08ea9baf9529
Sandy Maguire <sandy@sandymaguire.me>
2023-01-11 00:48:04 -0800

    poc: appimage

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,20 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.AppImage
+
+main =
+  defaultMainWithHooks
+    (simpleUserHooks
+      { postBuild =
+          appImageBuildHook
+            [ AppImage
+              { appName = "ld52-exe"
+              , appDesktop = "ld52.desktop"
+              , appIcons = ["./resources/textures/chicken.png"]
+              , appResources = [("./resources/textures/chicken.png", Nothing)]
+              , appDirCustomize = Nothing
+              }
+            ]
+      }
+    )
+
