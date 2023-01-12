2b7040e2fcbad66ff75f226102523a22329bf811
Andrew McKnight <thedonk@gmail.com>
2023-01-12 12:45:54 -0800

    removed dpos dependence on dt. increased max speed a bit to compensate

--- src/Game/Objects/Actor.hs
+++ src/Game/Objects/Actor.hs
@@ -11,12 +11,12 @@ actor
     :: OriginRect WorldPos
     -> SF (Bool, Double, V2 Double, V2 WorldPos, GlobalState) (V2 WorldPos)
 actor ore = loopPre 0 $
-  proc ((can_double, dt, vel'0, pos, gs), vel) -> do
+  proc ((can_double, _dt, vel'0, pos, gs), vel) -> do
     let collision = getCollisionMap gs
 
     let onGround = touchingGround (collision CollisionCheckGround) ore pos
     let vel' = updateVel (can_double || onGround) vel vel'0
-    let dpos = dt *^ vel'
+    let dpos = 0.016 *^ vel'
     let desiredPos = pos + coerce dpos
     let pos' = fromMaybe pos $ move collision (coerce ore) pos $ dpos
 
@@ -44,14 +44,14 @@ updateVelAir vel dvel =
     freeVel & _x %~ clampAbs maxXSpeed
   where
     grav = V2 0 10
-    maxXSpeed = 100
+    maxXSpeed = 110
     freeVel = vel + (dvel & _y %~ max 0) + grav
 
 updateVelGround :: V2 Double -> V2 Double -> V2 Double
 updateVelGround vel dvel@(V2 dvx _) =
     V2 (maxXSpeed * signum dvx) air_y
   where
-    maxXSpeed = 100
+    maxXSpeed = 110
     grav = V2 0 10
     (V2 _ air_y) = vel + dvel + grav
 
