cce878b0834404356846a583599989df9066d0df
Sandy Maguire <sandy@sandymaguire.me>
2023-01-16 17:24:10 -0800

    polish: change door color

--- src/Game/Objects/Door.hs
+++ src/Game/Objects/Door.hs
@@ -7,5 +7,5 @@ door :: V2 WorldPos -> OriginRect Double ->  V2 WorldPos -> Object
 door pos ore out
   = onHit (Just . fmap fst) (respondWith (TeleportOpportunity out))
   $ staticCollisionObject pos ore mempty
-  $ drawOriginRect (V4 192 128 0 255) ore
+  $ drawOriginRect (V4 34 15 4 255) ore
 
