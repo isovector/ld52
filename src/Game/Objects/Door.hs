37fb74f2f61c6713c9766d1569b597a4471ec135
Sandy Maguire <sandy@sandymaguire.me>
2023-01-16 17:17:31 -0800

    feat: door more opaque

--- src/Game/Objects/Door.hs
+++ src/Game/Objects/Door.hs
@@ -7,5 +7,5 @@ door :: V2 WorldPos -> OriginRect Double ->  V2 WorldPos -> Object
 door pos ore out
   = onHit (Just . fmap fst) (respondWith (TeleportOpportunity out))
   $ staticCollisionObject pos ore mempty
-  $ drawOriginRect (V4 192 128 0 64) ore
+  $ drawOriginRect (V4 192 128 0 255) ore
 
